779d05dbb0a18c573951c5ef40512ab0199f6212
Nick Barth <barth.nicholas@gmail.com>
2016-02-14 21:58:48 -0500

    finished poopmap

--- js/js.js
+++ js/js.js
@@ -46,6 +46,134 @@
   };
 })(jQuery);
 
+function initMap() {
+  var myLatLng = {lat: 42.886386, lng:  -78.878176};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: {lat: 42.892424, lng: -78.871549},
+    map: map,
+    title: 'Town Ballroom',
+    icon: 'favicon.ico'
+  });
+  var marker2 = new google.maps.Marker({
+    position: {lat: 42.915547, lng: -78.889803},
+    map: map,
+    title: 'Pho Dollar',
+    icon: 'favicon.ico'
+  });
+  var marker3 = new google.maps.Marker({
+    position: {lat: 42.938835, lng:  -78.882346},
+    map: map,
+    title: 'Wegmans',
+    icon: 'favicon.ico'
+  });
+  var marker4 = new google.maps.Marker({
+    position: {lat: 42.912071, lng: -78.876872},
+    map: map,
+    title: 'Thirsy Buffalo',
+    icon: 'favicon.ico'
+  });
+  var marker5 = new google.maps.Marker({
+    position: {lat: 42.894570, lng:  -78.868439},
+    map: map,
+    title: 'Work',
+    icon: 'favicon.ico'
+  });
+  var marker6 = new google.maps.Marker({
+    position: {lat: 42.964949, lng:  -78.772100},
+    map: map,
+    title: 'Parents',
+    icon: 'favicon.ico'
+  });
+  var marker7 = new google.maps.Marker({
+    position: {lat: 42.911461, lng: -78.877383},
+    map: map,
+    title: 'Milkies',
+    icon: 'favicon.ico'
+  });
+  var marker8 = new google.maps.Marker({
+    position: {lat: 40.761490, lng: -73.977632},
+    map: map,
+    title: 'MoMA',
+    icon: 'favicon.ico'
+  });
+  var marker9 = new google.maps.Marker({
+    position: {lat: 43.652580, lng: -79.398615},
+    map: map,
+    title: 'Chinatown Toronto',
+    icon: 'favicon.ico'
+  });
+  var marker10 = new google.maps.Marker({
+    position: {lat: 41.942873, lng: -87.677510},
+    map: map,
+    title: 'Chicago',
+    icon: 'favicon.ico'
+  });
+  var marker11 = new google.maps.Marker({
+    position: {lat: 40.437273, lng: -79.980915},
+    map: map,
+    title: 'Pittsburgh',
+    icon: 'favicon.ico'
+  });
+  var marker12 = new google.maps.Marker({
+    position: {lat: 33.466140, lng: -79.099388},
+    map: map,
+    title: 'Litchfield Beach',
+    icon: 'favicon.ico'
+  });
+  var marker13 = new google.maps.Marker({
+    position: {lat: 42.923203, lng: -78.876828},
+    map: map,
+    title: 'Cafe Aroma',
+    icon: 'favicon.ico'
+  });
+  var marker14 = new google.maps.Marker({
+    position: {lat: 42.276178, lng: -78.670873},
+    map: map,
+    title: 'Madigans',
+    icon: 'favicon.ico'
+  });
+  var marker15 = new google.maps.Marker({
+    position: {lat: 42.827938, lng: -78.696278},
+    map: map,
+    title: 'Columns',
+    icon: 'favicon.ico'
+  });
+  var marker16 = new google.maps.Marker({
+    position: {lat: 40.757283, lng: -73.965424},
+    map: map,
+    title: 'NYC Airbnb',
+    icon: 'favicon.ico'
+  });
+  var marker17 = new google.maps.Marker({
+    position: {lat: 42.894987, lng: -78.875933},
+    map: map,
+    title: 'Buffalo Club',
+    icon: 'favicon.ico'
+  });
+  var marker17 = new google.maps.Marker({
+    position: {lat: 42.903345, lng: -78.873207},
+    map: map,
+    title: 'CTG',
+    icon: 'favicon.ico'
+  });
+  var marker17 = new google.maps.Marker({
+    position: {lat: 43.021918, lng: -78.687792},
+    map: map,
+    title: 'Cousins',
+    icon: 'favicon.ico'
+  });
+
+
+
+
+}
+
 function UncheckAll() {
   var w = document.getElementsByTagName('input');
   for (var i = 0; i < w.length; i++) {
@@ -56,7 +184,6 @@ function UncheckAll() {
 }
 
 $(document).ready(function() {
-
   UncheckAll();
   $(":checkbox[value=total]").prop("checked", "true");
   $(".month-day").val("month");
